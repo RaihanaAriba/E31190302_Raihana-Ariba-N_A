1d044a1e2c8d8e2ad17ebe7fb2914353221d7a38
raihana Ariba N <raihana.arb@gmail.com>
2021-03-15 23:47:03 +0700

    minggu6

--- Minggu6/Minggu6/application/views/tugas2.php
+++ Minggu6/Minggu6/application/views/tugas2.php
@@ -85,7 +85,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <//?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div> -->
 
-	<h1>Hello!! Edo Kristian Yusak / E31192060 / Golongan C</h1>
+	<h1>Hello!! E31190302 / Raihana Ariba N / A </h1>
 
 </body>
 </html>
